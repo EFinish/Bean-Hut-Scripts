3042febbad1ca8935ab7a9ec834c956ea34c4c27
Armek <mperlwork@gmail.com>
2017-09-23 20:36:23 -0500

    Create beanhut.js

---
+++ beanhut.js
@@ -0,0 +1,3 @@
+var newImg = document.createElement("img");
+newImg.src = "https://cdn.discordapp.com/attachments/116705396093091844/361322091821400064/1506193551234.jpg";
+document.getElementById("wrap").insertBefore(newImg, document.getElementById("mainpage"));
