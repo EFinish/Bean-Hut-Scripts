2f7522eeabcae9773f7f1b7b655deee12bab9a6f
Mark Perl <mperlwork@gmail.com>
2017-09-23 21:07:44 -0500

    no mixed content

--- public/javascripts/beanhut.js
+++ public/javascripts/beanhut.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     var newImg = document.createElement("img");
-    newImg.src = "http://beanhuthost.herokuapp.com/images/benis-background.jpg";
+    newImg.src = "https://beanhuthost.herokuapp.com/images/benis-background.jpg";
     document.getElementById("wrap").insertBefore(newImg, document.getElementById("mainpage"));
 };
