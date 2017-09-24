6cdd6abc5a5ac71f1dcf5b4fa58117c68e77a362
Mark Perl <mperlwork@gmail.com>
2017-09-23 20:56:55 -0500

    renamed image

--- public/javascripts/beanhut.js
+++ public/javascripts/beanhut.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     var newImg = document.createElement("img");
-    newImg.src = "http://beanhuthost.herokuapp.com/images/1506193551234.jpg";
+    newImg.src = "http://beanhuthost.herokuapp.com/images/benis-background.jpg";
     document.getElementById("wrap").insertBefore(newImg, document.getElementById("mainpage"));
 };
