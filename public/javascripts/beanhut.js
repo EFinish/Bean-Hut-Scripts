c0c0bd89f36ccbbc65971f2d122b66185e9f1f5c
Mark Perl <mperlwork@gmail.com>
2017-09-23 21:10:02 -0500

    can't use onload with cytube

--- public/javascripts/beanhut.js
+++ public/javascripts/beanhut.js
@@ -1,5 +1,3 @@
-window.onload = function(){
-    var newImg = document.createElement("img");
-    newImg.src = "https://beanhuthost.herokuapp.com/images/benis-background.jpg";
-    document.getElementById("wrap").insertBefore(newImg, document.getElementById("mainpage"));
-};
+var newImg = document.createElement("img");
+newImg.src = "https://beanhuthost.herokuapp.com/images/benis-background.jpg";
+document.getElementById("wrap").insertBefore(newImg, document.getElementById("mainpage"));
